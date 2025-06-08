fc32945b212712993def30a5cf84043af9a707e9
abhijeet1592006 <abhijeet8800434205@gmail.com>
2025-06-08 20:00:57 +0530

    final updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,redirect,session,url_for,render_template
 
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO,join_room,emit
 
 app=Flask(__name__)
 
@@ -12,22 +12,41 @@ socketio=SocketIO(app)
 
 def main():
     if request.method=="POST":
+
         session['username']=(request.form["username"])
-        return render_template("chat.html")
+        username=session['username']
+
+        return render_template("chat.html",username=username)
 
     return render_template("index.html")
 @app.route("/chat",methods=["POST","GET"])
 def chat():
     if(session.get('username')!=None):
         if request.method=="POST":
-            session['message']=request.form['message']
-            print(session['message'])
-        return render_template("chat.html")
+            username=session['username']
+            
+        return render_template("chat.html",username=username)
 
 
     else:
         print(session.get("username"))
         return redirect(url_for("main"))
+    
+@socketio.on("status")
+def joinhandel(d):
+    print(d['data'])
+    user_name=session['username']
+    d1={"user_name":user_name}
+    socketio.emit("restatus",d1)
+
+
+@socketio.on("send")
+def message(u):
+    print(u['u'])
+    user_name=session['username']+":- "
+    
+    u1={'u':u['u'],"name":user_name}
+    socketio.emit("recive",u1)
 if __name__=="__main__":
     socketio.run(app,debug=True)
 
